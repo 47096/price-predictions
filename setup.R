64f88e0aa36d137c58ed22766d5565f12cea876b
samuel wong <21352640+47096@users.noreply.github.com>
2026-09-25 16:51:41 +1000

    Restore DataExplorer dependency; strip notebook dumps from analysis.R
    plot_missing was used without the package in setup. Remove giant
    glimpse/View console paste and stale comments.

--- setup.R
+++ setup.R
@@ -1,3 +1,3 @@
 #!/usr/bin/env Rscript
-packages <- c("corrplot", "DALEX", "Metrics", "modelStudio", "tidymodels", "tidyverse")
-install.packages(packages[!packages %in% installed.packages()[,"Package"]])
+packages <- c("corrplot", "DALEX", "DataExplorer", "Metrics", "modelStudio", "tidymodels", "tidyverse")
+install.packages(packages[!packages %in% installed.packages()[, "Package"]])
